1b237fdec4f61c0235111e1d371c1fd829b9f4fd
Juan David Merchan Torres <juan1014166@gmail.com>
2022-12-25 15:45:03 -0500

    feat(chapter2): Add strcon demo

---
+++ chapter2/strcondemo/go.mod
@@ -0,0 +1,5 @@
+module github.com/NikolaM-Dev/go-web/chapter2/strcondemo
+
+go 1.19
+
+require github.com/NikolaM-Dev/go-web/chapter2/strcon v0.0.0-20221225204034-45f0d90e7a0d // indirect
---
+++ chapter2/strcondemo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/NikolaM-Dev/go-web/chapter2/strcon"
+)
+
+func main() {
+	s := strcon.SwapCase("Gopher")
+	fmt.Println("Coverted string is:", s)
+}
